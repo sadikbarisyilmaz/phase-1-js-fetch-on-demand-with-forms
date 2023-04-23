9319c3f06195ed88e0fb1fc45cc5057447d72aa7
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-04-23 19:29:49 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-const init = () => {
-  
-}
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', ()=>{
+    
+  
+      
+    document.querySelector("form").addEventListener("submit", (e) => {
+        e.preventDefault()
+
+        let input = e.target.firstElementChild.nextElementSibling.value
+        console.log(e.target.firstElementChild.nextElementSibling.value);
+
+        fetch(`http://localhost:3000/movies/${input}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+            title.innerHTML = data["title"]
+            summary.innerHTML = data["summary"]
+        })
+    })
+    
+    
+});
